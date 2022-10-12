3ba42a63f3cec0fa7a107546d623aef17c1ed8bf
Daniel BOGADO <danistotal@hotmail.com>
2022-10-12 12:44:18 -0300

    modificacion nombre modelos

--- models/liquidacion.py
+++ models/liquidacion.py
@@ -2,7 +2,7 @@ from odoo import models,fields,api
 
 
 class liquidacion(models.Model):
-    _name = 'liquidacion.liquidacion'
+    _name = 'pago_proveedores.liquidacion'
     proveedor = fields.Char(string="Proveedor")
     monto = fields.Char(string="Monto total")
     fecha = fields.Date(string="Fecha Liquidacion")
