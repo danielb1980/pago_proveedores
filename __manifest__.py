aac6e82483196200da9478355b9ad2aa19af68f9
Daniel BOGADO <danistotal@hotmail.com>
2022-10-17 12:25:38 -0300

    menu proveedores y liquidaciones

--- __manifest__.py
+++ __manifest__.py
@@ -18,7 +18,7 @@
     'version': '1.1',
 
     # any module necessary for this one to work correctly
-    'depends': ['base', 'mail'],
+    'depends': ['base', 'mail','towing_service'],
 
     # always loaded
     'data': [
--- models/liquidacion.py
+++ models/liquidacion.py
@@ -1,9 +1,20 @@
-from odoo import models,fields,api
+from odoo import _,models,fields,api
 
 
 class liquidacion(models.Model):
-    _name = 'pago_proveedores.liquidacion'
+    _name = 'pagoproveedores.liquidacion'
     proveedor = fields.Char(string="Proveedor")
     monto = fields.Char(string="Monto total")
     fecha = fields.Date(string="Fecha Liquidacion")
-    
+    partner_id = fields.Many2one(
+        "res.partner",
+        string=_("Partner"),
+        required=True,
+        index=True,
+    )
+    partner_id2 = fields.Many2one(
+        "res.partner",
+        string="Partner",
+        required=True,
+        index=True,
+    )
