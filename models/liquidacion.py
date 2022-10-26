9bec2df0df6bf9e9aa1947462a2a68ee32d93746
Julio Santa Cruz <bartacruz@gmail.com>
2022-10-26 12:36:49 -0300

    Estado default

--- models/liquidacion.py
+++ models/liquidacion.py
@@ -17,9 +17,6 @@ class liquidacion(models.Model):
     purchase_move_ids= fields.One2many("purchase.move","liquidacion_id",string=_("Facturas"),required=True,readonly=True)
     currency_id = fields.Many2one(related='company_id.currency_id', depends=["company_id"], store=True, ondelete="restrict")
     
-    # Usar partner_id para identificar al proveedor
-    #proveedor = fields.Char(string="Proveedor") 
-    
     monto = fields.Monetary(string="Monto total")
     fecha = fields.Date(string="Fecha Liquidacion", default=fields.Datetime.now)
     
@@ -27,8 +24,9 @@ class liquidacion(models.Model):
         "res.partner",
         string=_("Partner"),
         required=True,
-        index=True
+        index=True,
+        ondelete="restrict"
     )
    
-    estado = fields.Selection([("B", "Borrador"),("P", "Pendiente"),("C", "Controlado"),("A", "Aprobado"),("P", "Pagado")])
+    estado = fields.Selection([("B", "Borrador"),("P", "Pendiente"),("C", "Controlado"),("A", "Aprobado"),("P", "Pagado")], default="B")
     observaciones = fields.Text(string="Observaciones")
