dea80e16984ec58419ddeb5c6d35258657644396
Daniel BOGADO <danistotal@hotmail.com>
2022-10-26 11:23:56 -0300

    Notebook Page Facuras liquidadas readonly

--- models/liquidacion.py
+++ models/liquidacion.py
@@ -14,7 +14,7 @@ class liquidacion(models.Model):
         index=True,
         default=lambda self: self.env.user.company_id
     )
-    purchase_move_ids= fields.One2many("purchase.move","id",string=_("Facturas"),required=True)
+    purchase_move_ids= fields.One2many("purchase.move","liquidacion_id",string=_("Facturas"),required=True,readonly=True)
     currency_id = fields.Many2one(related='company_id.currency_id', depends=["company_id"], store=True, ondelete="restrict")
     
     # Usar partner_id para identificar al proveedor
@@ -29,12 +29,6 @@ class liquidacion(models.Model):
         required=True,
         index=True
     )
-    LiquidacionLine_ids=fields.One2many("pago_proveedores.liquidacion.line","liquidacion_id",_("Facturas"))
-    estado = fields.Selection([("B", "Borrador"),("A", "Aprobado")])
+   
+    estado = fields.Selection([("B", "Borrador"),("P", "Pendiente"),("C", "Controlado"),("A", "Aprobado"),("P", "Pagado")])
     observaciones = fields.Text(string="Observaciones")
-class LiquidacionLine(models.Model):
-    _name = 'pago_proveedores.liquidacion.line'
-    factura=fields.Many2one("purchase.move",string="Factura")
-      
-    liquidacion_id=fields.Many2one("pago_proveedores.liquidacion",string="Liquidacion")
-    
